ad69993ca6313f3d47791fd7040da703e27aaa22
thosou <thomas.souvignet@gmx.fr>
2019-01-05 18:08:46 +0100

    added all caps and switches from the original file

--- pyseeman/techlib.py
+++ pyseeman/techlib.py
@@ -36,31 +36,136 @@ class ITRS45cap:
     rating = 1.2
 
 class ITRS32cap:
-    pass
+    """
+    32 nm Oxide Capacitors
+    """
+    tech_name = 'ITRS 32nm'
+    dev_name = 'ITRS 32nm Oxide Capacitor'
+    capacitance = (40 ** 2) * .8e-15 * .45 / .032
+    area = 40e-6 * 40e-6
+    bottom_cap = 135.3e-15
+    esr = 0
+    rating = 1.2
 
 class ITRS22cap:
-    pass
+    """
+    22 nm Oxide Capacitors
+    """
+    tech_name = 'ITRS 22nm'
+    dev_name = 'ITRS 22nm Oxide Capacitor'
+    capacitance = (40 ** 2) * .58e-15 * .4 / .022
+    area = 40e-6 * 40e-6
+    bottom_cap = 135.3e-15
+    esr = 0
+    rating = 1.2
+
 
 class ITRS16cap:
-    pass
+    """
+    16 nm Oxide Capacitors
+    """
+    tech_name = 'ITRS 16nm'
+    dev_name = 'ITRS 16nm Oxide Capacitor'
+    capacitance = (40 ** 2) * .48e-15 * .35 / .016
+    area = 40e-6 * 40e-6
+    bottom_cap = 135.3e-15
+    esr = 0
+    rating = 1.2
 
+#TODO check the name and type of that cap
 class HVcap:
-    pass
+    """
+    16 nm Oxide Capacitors
+    """
+    tech_name = 'ITRS 16nm'
+    dev_name = 'ITRS 16nm Oxide Capacitor'
+    capacitance = (40 ** 2) * .48e-15 * .35 / .016 * 0.01
+    area = 40e-6 * 40e-6
+    bottom_cap = 135.3e-15
+    esr = 0
+    rating = 1.2
 
 class ITRS90sw:
-    pass
+    """
+    90 nm switch
+    """
+    tech_name = 'ITRS 90 nm'
+    dev_name = 'ITRS 90 nm NMOS native transistor';
+    area = 1e-6 * 90e-9 / .4
+    conductance = 1.11e-3
+    gate_rating = 1.25
+    drain_rating = 1.25
+    gate_cap = 1e-15
+    drain_cap = .33 * gate_cap
+    body_cap = .2 * gate_cap
 
 class ITRS65sw:
-    pass
+    """
+    65 nm switch
+    """
+    tech_name = 'ITRS 65 nm'
+    dev_name = 'ITRS 65 nm NMOS native transistor';
+    area = 1e-6 * 65e-9 / .35
+    conductance = 1.3e-3
+    gate_rating = 1.25
+    drain_rating = 1.25
+    gate_cap = 0.9e-15
+    drain_cap = .33 * gate_cap
+    body_cap = .2 * gate_cap
 
 class ITRS45sw:
-    pass
+    """
+    45 nm switch
+    """
+    tech_name = 'ITRS 45 nm'
+    dev_name = 'ITRS 45 nm NMOS native transistor';
+    area = 1e-6 * 45e-9 / .35
+    conductance = 1.51e-3
+    gate_rating = 1.25
+    drain_rating = 1.25
+    gate_cap = 0.84e-15
+    drain_cap = .33 * gate_cap
+    body_cap = .2 * gate_cap
 
 class ITRS32sw:
-    pass
+    """
+    32 nm switch
+    """
+    tech_name = 'ITRS 32 nm'
+    dev_name = 'ITRS 32 nm NMOS native transistor';
+    area = 1e-6 * 32e-9 / .35
+    conductance = 1.82e-3
+    gate_rating = 1.25
+    drain_rating = 1.25
+    gate_cap = 0.8e-15
+    drain_cap = .33 * gate_cap
+    body_cap = .2 * gate_cap
 
 class ITRS22sw:
-    pass
+    """
+    22 nm switch
+    """
+    tech_name = 'ITRS 22 nm'
+    dev_name = 'ITRS 22 nm NMOS native transistor';
+    area = 1e-6 * 22e-9 / .3
+    conductance = 2.245e-3
+    gate_rating = 1.25
+    drain_rating = 1.25
+    gate_cap = 0.58e-15
+    drain_cap = .33 * gate_cap
+    body_cap = .2 * gate_cap
+
 
 class ITRS16sw:
-    pass
+    """
+    16 nm switch
+    """
+    tech_name = 'ITRS 16 nm'
+    dev_name = 'ITRS 16 nm NMOS native transistor';
+    area = 1e-6 * 16e-9 / .3
+    conductance = 2.535e-3
+    gate_rating = 1.25
+    drain_rating = 1.25
+    gate_cap = 0.48e-15
+    drain_cap = .33 * gate_cap
+    body_cap = .2 * gate_cap
