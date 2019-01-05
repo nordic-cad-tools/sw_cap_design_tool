dddbf94ee81320b41797e621f1fbb278c542840a
thosou <thomas.souvignet@gmx.fr>
2019-01-05 15:47:21 +0100

    initialization of the package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='sw_cap_design_tool',
+    version='0.0.0',
+    packages=['pyseeman'],
+    url='',
+    license='',
+    author='thosou',
+    author_email='',
+    description=''
+)
