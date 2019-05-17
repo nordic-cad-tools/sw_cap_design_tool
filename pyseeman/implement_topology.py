55bf0e6edb5e3313b951ec23e61f01b1c198db35
thosou <thomas.souvignet@gmx.fr>
2019-05-17 16:23:30 +0200

    added first for loop with python implementation

--- pyseeman/implement_topology.py
+++ pyseeman/implement_topology.py
@@ -1,4 +1,4 @@
-def implement_topology(topology, Vin, switchTechs, capTechs, compMetric=1)
+def implement_topology(topology, Vin, switchTechs, capTechs, compMetric=1):
     """
     implement_topology: matches components with switches and components in
     the topology.
@@ -19,3 +19,32 @@ def implement_topology(topology, Vin, switchTechs, capTechs, compMetric=1)
     vr = topology.vr * Vin
     vcb = topology.vcb * Vin
     vrb = topology.vrb * Vin
+
+    switch_assign = []
+    cap_assign = []
+    switch_rel_size = []
+    cap_rel_size = []
+
+    # Assign Capacitors
+    for i in range(ac.shape[1]):
+        print(i)
+        Mc = 0
+        Cc = 0 # cap cost
+        for j, cap_tech in enumerate(capTechs):
+            if vc(i) <= cap_tech:
+                # Cap could work ... let's see if it's good
+                # Use area-limited metric, which is usually applicable
+                C = cap_tech.area
+                M = cap_tech.capacitance*vc(i)**2 / C
+                if (M > Mc):
+                    if (Mc == 0):
+                        cap_assign = [cap_assign, cap_tech]
+                    else:
+                        cap_assign.append(cap_tech)
+
+                    Mc = M
+                    Cc = C
+
+
+if __name__ == '__main__':
+    pass
