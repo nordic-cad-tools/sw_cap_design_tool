f456cafd4c22f4763c2aa25899db98f621345819
thosou <thomas.souvignet@gmx.fr>
2019-05-17 16:03:45 +0200

    created initial function

---
+++ pyseeman/implement_topology.py
@@ -0,0 +1,21 @@
+def implement_topology(topology, Vin, switchTechs, capTechs, compMetric=1)
+    """
+    implement_topology: matches components with switches and components in
+    the topology.
+    implementation = implement_topology(topology, Vin, switchTechs, capTechs, compMetric)
+       topology: structure created by generate_topology
+       Vin: input voltage of converter
+       switchTechs: an array of technology structures available for switch use
+       capTechs: an array of technology structures available for cap use
+       compMetric: a metric (1=area, 2=loss) used for determining the best
+                   component (1=default)
+    """
+
+    #Break out components of topology structure
+    ratio = topology.ratio
+    ac = topology.ac
+    ar = topology.ar
+    vc = topology.vc * Vin
+    vr = topology.vr * Vin
+    vcb = topology.vcb * Vin
+    vrb = topology.vrb * Vin
