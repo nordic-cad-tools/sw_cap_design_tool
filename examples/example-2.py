9800cd7e50a6dbdbc72d8c7459d30a057a9ea84e
thosou <thomas.souvignet@gmx.fr>
2019-06-11 10:39:33 +0200

    added switch and cap object in exmaple-2

--- examples/example-2.py
+++ examples/example-2.py
@@ -1,4 +1,4 @@
-from pyseeman.techlib import ITRS16cap, ITRS16sw
+from pyseeman.techlib import ITRS16cap, ITRS16sw, Switch, Capacitor
 from pyseeman.core import Topology, plot_regulation
 import numpy as np
 
@@ -17,11 +17,30 @@ vout = np.linspace(0, 1.2, 1000).reshape((1, 1000))
 plot_regulation(topologies, 1.2, vout, 500e-3, 1e-7, [ITRS16sw], [ITRS16cap])
 
 # vin sweep
-ITRS16cap.rating = 3
-ITRS16sw.drain_rating = 3
-ITRS16sw.gate_rating = 3
+hv_switch = Switch(
+    tech_name="HV tech",
+    dev_name="HV switch",
+    area=100e-6 * 100e-6,
+    conductance=1e-3,
+    gate_rating=5.0,
+    drain_rating=5.0,
+    gate_cap=10e-15,
+    drain_cap=1e-15,
+    body_cap=1e-15,
+)
+
+hv_cap = Capacitor(
+    tech_name="HV tech",
+    dev_name="HV cap",
+    capacitance=20e-12,
+    area=100e-6 * 100e-6,
+    bottom_cap=100e-15,
+    esr=0,
+    rating=5,
+)
+
 
 vin = np.linspace(1, 5, 1000).reshape((1, 1000))
-plot_regulation(topologies, vin, 1.2, 100e-3, 1e-6, [ITRS16sw], [ITRS16cap])
+plot_regulation(topologies, vin, 1.2, 100e-3, 1e-6, [hv_switch], [hv_cap])
 
 plt.show()
