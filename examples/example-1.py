354e827f14e7f0f3bad11f0117f433b7842e2714
thosou <thomas.souvignet@gmx.fr>
2019-06-11 09:44:59 +0200

    black formatting example-1

--- examples/example-1.py
+++ examples/example-1.py
@@ -5,7 +5,9 @@ import matplotlib.pyplot as plt
 # create the ladder topology with a 3:2 ratio
 ladder = Topology("Ladder", 2, 3)
 # implement the topology
-ladder_imp = ladder.implement(vin=1.5, switch_techs=[ITRS16sw], cap_techs=[ITRS16cap], comp_metric=1)
+ladder_imp = ladder.implement(
+    vin=1.5, switch_techs=[ITRS16sw], cap_techs=[ITRS16cap], comp_metric=1
+)
 # plot the efficiency contour
 ax1 = plot_opt_contour(ladder_imp, Vin=1.5, Iout=10e-3, Ac=1e-6)
 plt.show()
